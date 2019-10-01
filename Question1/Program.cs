8792a04f0f83ab4c43724deb3d1c1e12628a8920
s00189392 <s00189392@LAB-B1041-14>
2019-10-01 12:54:40 +0100

    Finished the class

--- Question1/Program.cs
+++ Question1/Program.cs
@@ -17,16 +17,43 @@ namespace Question1
             c1.EngineSize = 1.6;
 
             Car c2 = new Car();
-            c1.Make = "Opel";
-            c1.Model = "Astra";
-            c1.CurrentSpeed = 0;
-            c1.EngineSize = 1.2;
+            c2.Make = "Opel";
+            c2.Model = "Astra";
+            c2.CurrentSpeed = 0;
+            c2.EngineSize = 1.2;
 
-            //WriteLine($"The {c1.Make}")
+            WriteLine($"The {c1.Make} {c1.Model}, Engine size: {c1.EngineSize}l is currently travelling at {c1.CurrentSpeed} km/h");
+
+            WriteLine($"The {c2.Make} {c2.Model}, Engine size: {c2.EngineSize}l is currently travelling at {c2.CurrentSpeed} km/h");
         }
     }
     class Car
     {
+        private string _make;
 
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+        private string _model;
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        private int _currentSpeed;
+        public int CurrentSpeed
+        {
+            get { return _currentSpeed;}
+            set { _currentSpeed = value; }
+        }
+        private double _enginSize;
+
+        public double EngineSize
+        {
+            get { return _enginSize; }
+            set { _enginSize = value; }
+        }
     }
 }
